6a78f920d9f2ba30c46434d9d895fe3b37de73fb
star77777 <104746184+star77777@users.noreply.github.com>
2022-07-23 15:07:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/travelplan_finalfianl/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.travelplan_finalfianl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
